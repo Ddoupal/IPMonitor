e01b267e5f763bfa5f162a8fe878c5e318c5f047
ddoupal <doupal@cuttersystems.cz>
2025-03-05 09:06:46 +0100

    - init commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IpMonitor;
+
+internal class Program
+{
+    private const string xmlFilePath = "IpsAvailabilityResults.xml";
+
+    public static void Main()
+    {
+
+    }
+
+    private static async Task PingMonitorAsync()
+    {
+
+    }
+
+    private static async Task WriteResultsAsync()
+    {
+
+    }
+
+    private static async Task ReadResultsAsync()
+    {
+
+    }
+}
